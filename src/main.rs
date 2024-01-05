74c35d3a70a0cd8c44f686df126f0a9c390cf8aa
wilzet <129424713+wilzet@users.noreply.github.com>
2024-01-05 17:33:49 +0100

    Docs and tests (#3)
    * Total score calculation
    
    Even though games are being run in parallel, total scores for each player is calculated.
    The total scores are also printed.
    
    * Added tests
    
    Tests added to main.rs
    
    * Added program tests
    
    Tests for programs. They are not comprehensive but should be somewhat comprehensive in accordance to each strategy.
    
    * Added tests to game
    
    Apart from simple unit tests, &str is now used instead of String for the Player name.
    
    * Updated game.rs
    
    Programs are now obligatory for Players
    
    * Added documentation
    
    Some restructuring and added documentation to game.rs

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,30 @@
+use std::sync::{Arc, Mutex};
 use threadpool::ThreadPool;
 use rand::{prelude::*, distributions};
+use tourney::game::{Player, play};
+use tourney::programs::*;
 
-mod game;
-mod programs;
-
-use game::{Player, play};
-use programs::*;
-
-const MIN_ROUNDS: u32 = 90;
+const MIN_ROUNDS: u32 = 70;
 const MAX_ROUNDS: u32 = 100;
 
 fn main() {
-    if MIN_ROUNDS > MAX_ROUNDS {
-        panic!("MIN_ROUNDS cannot be less than MAX_ROUNDS");
-    }
-
     println!("Program start\n");
     let pool = ThreadPool::with_name("Games".into(), 20);
 
     let players = vec![
-        Player::with_name_and_program("Take back 1".into(), take_back),
-        Player::with_name_and_program("Friendly".into(), friendly),
-        Player::with_name_and_program("Evil".into(), evil),
-        Player::with_name_and_program("Tit for tat".into(), tit_for_tat),
-        Player::with_name_and_program("Tit for two tats".into(), tit_for_two_tats),
-        Player::with_name_and_program("Greedy and friendly".into(), greedy_blue_and_friendly),
-        Player::with_name_and_program("Greedy and evil".into(), greedy_blue_and_evil),
+        Player::with_name("Take back 1", take_back),
+        Player::with_name("Friendly", friendly),
+        Player::with_name("Evil", evil),
+        Player::with_name("Tit for tat", tit_for_tat),
+        Player::with_name("Tit for two tats", tit_for_two_tats),
+        Player::with_name("Greedy and friendly", greedy_blue_and_friendly),
+        Player::with_name("Greedy and evil", greedy_blue_and_evil),
     ];
 
-    let rounds = distributions::Uniform::from(MIN_ROUNDS..MAX_ROUNDS+1).sample(&mut rand::thread_rng());
+    let scores = init_scores(players.len());
+    let rounds = random_rounds(MIN_ROUNDS, MAX_ROUNDS);
 
-    println!("{rounds} rounds!");
-    println!("Pairing every program... ({0} matches)\n", players.len() * (players.len() + 1) / 2);
+    println!("Pairing every program... ({0} games)\n", players.len() * (players.len() + 1) / 2);
 
     for i in 0..players.len() {
         for j in 0..players.len() {
@@ -39,20 +32,130 @@ fn main() {
                 continue;
             }
 
-            add_game(players[i].clone(), players[j].clone(), &pool, rounds);
+            add_game(
+                players[i].clone(),
+                players[j].clone(),
+                &pool,
+                rounds,
+                (scores[i].clone(), scores[j].clone()),
+            );
         }
     }
 
     pool.join();
+
+    let mut scores = scores.iter()
+        .enumerate()
+        .map(|(i, v)| (*v.lock().unwrap(), players[i].get_name()))
+        .collect::<Vec<_>>();
+    scores.sort();
+
+    println!("{rounds} rounds!");
+    for (i, v) in scores.iter().rev().enumerate() {
+        println!("{0}. {2} - {1:.2}", i + 1, v.0 as f32 / players.len() as f32, v.1);
+    } 
+
     println!("\nProgram end");
 }
 
-fn add_game(player1: Player, player2: Player, pool: &ThreadPool, rounds: u32) {
+fn random_rounds(min_rounds: u32, max_rounds: u32) -> u32 {
+    if min_rounds > max_rounds {
+        panic!("max_rounds cannot be less than min_rounds");
+    }
+
+    if min_rounds == 0 {
+        panic!("min_rounds cannot not be 0");
+    }
+
+    distributions::Uniform::from(min_rounds..max_rounds+1).sample(&mut rand::thread_rng())
+}
+
+fn init_scores(length: usize) -> Vec<Arc<Mutex<i32>>> {
+    if length == 0 {
+        panic!("Cannot initialize with a length of 0");
+    }
+
+    let mut scores = Vec::new();
+    for _ in 0..length {
+        scores.push(Arc::new(Mutex::new(0)));
+    }
+    
+    scores
+}
+
+fn add_game(player1: Player, player2: Player, pool: &ThreadPool, rounds: u32, score_totals: (Arc<Mutex<i32>>, Arc<Mutex<i32>>)) {
     pool.execute(move || {
         let name = format!("{0:>20}  vs.  {1:<20}", player1.get_name(), player2.get_name());
-        let score = play(player1, player2, rounds);
+        let scores = play(player1, player2, rounds);
 
-        let output = format!("{0}   {1:>3} - {2:<3}", name, score.0, score.1);
+        let output = format!("{0}   {1:>3} - {2:<3}", name, scores.0, scores.1);
         println!("{output}\n");
+
+        if let Ok(mut score) = score_totals.0.lock() {
+            *score += scores.0;
+        }
+
+        if let Ok(mut score) = score_totals.1.lock() {
+            *score += scores.1;
+        }
     });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_game_test() {
+        let pool = ThreadPool::with_name("Games".into(), 20);
+
+        let players = vec![
+            Player::with_name("1", greedy_blue_and_evil),
+            Player::with_name("2", take_back),
+        ];
+
+        let scores = init_scores(players.len());
+
+        add_game(
+            players[0].clone(),
+            players[1].clone(),
+            &pool,
+            10,
+            (scores[0].clone(), scores[1].clone())
+        );
+
+        pool.join();
+
+        let mut scores = scores.iter()
+            .enumerate()
+            .map(|(i, v)| (*v.lock().unwrap(), players[i].get_name()))
+            .collect::<Vec<_>>();
+        scores.sort();
+
+        assert_eq!(scores, [(5, "2"), (36, "1")]);
+    }
+
+    #[test]
+    #[should_panic(expected = "length")]
+    fn init_scores_test() {
+        init_scores(0);
+    }
+
+    #[test]
+    #[should_panic(expected = "less than")]
+    fn random_rounds_test_0() {
+        random_rounds(10, 1);
+    }
+
+    #[test]
+    #[should_panic(expected = "be 0")]
+    fn random_rounds_test_1() {
+        random_rounds(0, 0);
+    }
+
+    #[test]
+    fn random_rounds_test_2() {
+        assert!(random_rounds(10, 10) == 10);
+        assert!(random_rounds(10, 11) < 12);
+    }
 }

8cc7a97b098e5a813c4953778e53a134f8052ae2
Wilmer Z <wilzet02@gmail.com>
2024-01-05 13:10:05 +0100

    Total score calculation
    Even though games are being run in parallel, total scores for each player is calculated.
    The total scores are also printed.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::{Arc, Mutex};
 use threadpool::ThreadPool;
 use rand::{prelude::*, distributions};
 
@@ -7,7 +8,7 @@ mod programs;
 use game::{Player, play};
 use programs::*;
 
-const MIN_ROUNDS: u32 = 90;
+const MIN_ROUNDS: u32 = 70;
 const MAX_ROUNDS: u32 = 100;
 
 fn main() {
@@ -28,10 +29,16 @@ fn main() {
         Player::with_name_and_program("Greedy and evil".into(), greedy_blue_and_evil),
     ];
 
+    let mut scores = Vec::new();
+    for _ in 0..players.len() {
+        scores.push(Arc::new(Mutex::new(0)));
+    }
+    let scores = scores;
+
     let rounds = distributions::Uniform::from(MIN_ROUNDS..MAX_ROUNDS+1).sample(&mut rand::thread_rng());
 
     println!("{rounds} rounds!");
-    println!("Pairing every program... ({0} matches)\n", players.len() * (players.len() + 1) / 2);
+    println!("Pairing every program... ({0} games)\n", players.len() * (players.len() + 1) / 2);
 
     for i in 0..players.len() {
         for j in 0..players.len() {
@@ -39,20 +46,39 @@ fn main() {
                 continue;
             }
 
-            add_game(players[i].clone(), players[j].clone(), &pool, rounds);
+            add_game(players[i].clone(), players[j].clone(), &pool, rounds, (scores[i].clone(), scores[j].clone()));
         }
     }
 
     pool.join();
+
+    let mut scores = scores.iter()
+        .enumerate()
+        .map(|(i, v)| (*v.lock().unwrap(), players[i].get_name()))
+        .collect::<Vec<_>>();
+    scores.sort();
+
+    for (i, v) in scores.iter().rev().enumerate() {
+        println!("{0}. {2} - {1:.2}", i + 1, v.0 as f32 / players.len() as f32, v.1);
+    } 
+
     println!("\nProgram end");
 }
 
-fn add_game(player1: Player, player2: Player, pool: &ThreadPool, rounds: u32) {
+fn add_game(player1: Player, player2: Player, pool: &ThreadPool, rounds: u32, score_totals: (Arc<Mutex<i32>>, Arc<Mutex<i32>>)) {
     pool.execute(move || {
         let name = format!("{0:>20}  vs.  {1:<20}", player1.get_name(), player2.get_name());
-        let score = play(player1, player2, rounds);
+        let scores = play(player1, player2, rounds);
 
-        let output = format!("{0}   {1:>3} - {2:<3}", name, score.0, score.1);
+        let output = format!("{0}   {1:>3} - {2:<3}", name, scores.0, scores.1);
         println!("{output}\n");
+
+        if let Ok(mut score) = score_totals.0.lock() {
+            *score += scores.0;
+        }
+
+        if let Ok(mut score) = score_totals.1.lock() {
+            *score += scores.1;
+        }
     });
 }

02f6dc0e95673ca0a707633c3a547dceb76ebb1f
Wilmer Z <wilzet02@gmail.com>
2024-01-18 14:01:25 +0100

    New program

--- src/programs/strategic.rs
+++ src/programs/strategic.rs
@@ -42,3 +42,45 @@ pub fn try_to_guess(last_moves: &[Move]) -> Color {
         }
     }
 }
+
+fn some_greed_and_match_opponent(last_moves: &[Move], greedy_rounds: usize) -> Color {
+    // Try to be greedy early
+    if last_moves.len() < greedy_rounds {
+        let blue_count = last_moves.iter()
+            .fold((0, 0), |acc, m| {
+                match m {
+                    (Color::Blue, Color::Blue) => (acc.0 + 1, acc.1 + 1),
+                    (Color::Blue, _) => (acc.0 + 1, acc.1),
+                    (_, Color::Blue) => (acc.0, acc.1 + 1),
+                    _ => acc,
+                }
+            });
+
+        // Want blue_count difference of 2
+        if blue_count.0 - 1 <= blue_count.1 {
+            return Color::Blue;
+        }
+    }
+
+    let (red_count, green_count, blue_count) = last_moves.iter()
+        .fold((0, 0, 0), |(r, g, b), m| match m.1 {
+            Color::Red => (r + 1, g, b),
+            Color::Green => (r, g + 1, b),
+            Color::Blue => (r, g, b + 1),
+        });
+
+    // If opponent chooses mostly green or red, choose that too.
+    // Else compare with blue, and choose randomly
+    match red_count.cmp(&green_count) {
+        cmp::Ordering::Less => Color::Green,
+        cmp::Ordering::Equal => match red_count.cmp(&blue_count) {
+            cmp::Ordering::Equal => *[Color::Red, Color::Green, Color::Blue].choose(&mut rand::thread_rng()).unwrap(),
+            _ => *[Color::Red, Color::Green].choose(&mut rand::thread_rng()).unwrap(),
+        }
+        cmp::Ordering::Greater => Color::Red,
+    }
+}
+
+pub fn greed_first_15(last_moves: &[Move]) -> Color {
+    some_greed_and_match_opponent(last_moves, 15)
+}
